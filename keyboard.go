89d5e2085974c5f417fa9fb7d3787914a00fd3ee
Aadil Nabi <aadilnabi@Aadils-MacBook-Pro.local>
2023-05-17 18:30:00 +0530

    added commnents

--- keyboard.go
+++ keyboard.go
@@ -13,6 +13,7 @@ func GetInput() int {
 
 	fmt.Println("Enter the Number")
 
+	// Create a new reader to read data from the terminal
 	reader := bufio.NewReader(os.Stdin)
 	input, err := reader.ReadString('\n')
 
@@ -20,8 +21,10 @@ func GetInput() int {
 		log.Fatal(err)
 	}
 
+	// Trim the white space that comes when user hits enter key
 	trimmedNumber := strings.TrimSpace(input)
 
+	// Atoi to parse the string into an int
 	num, err := strconv.Atoi(trimmedNumber)
 	if err != nil {
 		log.Fatal(err)
